4dd485952a2ace1521f35c1b7631ca5c3e26c0c1
Paul Barrett <fastturtle4@gmail.com>
2018-05-20 22:19:39 -0400

    trying to add navigation bar
    Having some trouble getting navigation bar to work. I've got to fix some errors

--- app/src/main/java/me/pbarrett/happybirthday/MainActivity.java
+++ app/src/main/java/me/pbarrett/happybirthday/MainActivity.java
@@ -5,9 +5,28 @@ import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ListView mDrawerList;
+    private ArrayAdapter<String> mAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mDrawerList = (ListView)findViewById(R.id.navList);
+
+        addDrawerItems();
+    }
+
+    private void addDrawerItems() {
+        String[] osArray = { "Android", "iOS", "Windows", "OS X", "Linux" };
+        mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+        mDrawerList.setAdapter(mAdapter);
+
+        mDrawerList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(MainActivity.this, "Time for an upgrade!", Toast.LENGTH_SHORT).show();
+            }
+        });
     }
 }
+
